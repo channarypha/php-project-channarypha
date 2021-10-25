a4d0f0b3816e89995d41b077c341e2f38f723dd4
channarypha <channary.pha@student.passerellesnumeriques.org>
2021-10-26 00:59:50 +0700

    1

--- pages/about.php
+++ pages/about.php
@@ -12,9 +12,9 @@
             <img src="assets/images/<?=$product['image']?>" alt="">
             
             <div class="content" >
-                <h3 style="color: green;">limited offer</h3>
-                <p style="color: pink;">upto <?=$product['discount']?> off</p>
-                <p style="color: #AE00FB;"><?=$description?>...<a href="detail_pro.php?id=<?=$product['product_id']?>" class="btn" style="background-color:brown;font-size:150%">check out</a></p>
+                <h3 style="color: purple;">limited offer</h3>
+                <p style="color: #2C394B;">upto <?=$product['discount']?> off</p>
+                <p style="color: #055052"><?=$description?>...<a href="detail_pro.php?id=<?=$product['product_id']?>" class="btn" style="background-color:brown;font-size:150%">check out</a></p>
             </div>
             
         </div>
